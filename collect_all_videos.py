8b281dd58d7c9cf945ff4adfdffb4d3a72b9a085
Jonah Eaton <49370794+jonaheaton@users.noreply.github.com>
2024-06-17 10:36:30 -0400

    determine the outliers and assign them nan values, split the full data set into train, val and test sets

--- collect_all_videos.py
+++ collect_all_videos.py
@@ -11,6 +11,66 @@ from video import main_process_video, slow_down_video, create_video_with_ellipse
 from prep_data import get_hula_hoop_coord, prep_for_lstm, save_csv
 
 
+def determine_outlier_rows(df, outlier_th=3):
+
+    ellipse_cols = [col for col in df.columns if 'ellipse' in col]
+    print(ellipse_cols)
+    # df_vals = (df.drop(['frame','video_id','file_name'], axis=1)).values
+    df_vals = df[ellipse_cols].values
+    df_vals = StandardScaler().fit_transform(df_vals)
+
+    outlier_rows = np.any(np.abs(df_vals) > outlier_th, axis=1)
+    print('total number of rows: ', len(outlier_rows))
+    print('number of outliers: ', np.sum(outlier_rows))
+    df.loc[outlier_rows,ellipse_cols] = np.nan
+    return df
+
+
+
+def assign_data_types(df):
+    # Ensure randomness is reproducible
+    np.random.seed(42)
+    
+    video_ids_ordered_by_size = df['video_id'].value_counts().index.tolist()
+    large_videos = video_ids_ordered_by_size[:int(len(video_ids_ordered_by_size)/2)]
+    small_videos = video_ids_ordered_by_size[int(len(video_ids_ordered_by_size)/2):]
+
+    # Shuffle the video_id to ensure random selection
+
+    np.random.shuffle(large_videos)
+    np.random.shuffle(small_videos)
+
+    # Select video IDs for testing and validation
+    test_ids = large_videos[:1] + small_videos[:1]
+    validation_ids = large_videos[1:2] + small_videos[1:2]
+    training_ids = large_videos[2:] + small_videos[2:]
+    print('test_ids: ', test_ids)
+    print('validation_ids: ', validation_ids)
+    print('training_ids: ', training_ids)
+    
+    # Initialize the new column with empty strings
+    df['data_type'] = ''
+    
+    # Assign 'test' and 'validation' to the selected video IDs
+    df.loc[df['video_id'].isin(test_ids), 'data_type'] = 'test'
+    df.loc[df['video_id'].isin(validation_ids), 'data_type'] = 'validation'
+    
+    # Process the remaining videos for training and validation split
+    for video_id in training_ids:
+        video_df = df[df['video_id'] == video_id]
+        val_split_index = int(len(video_df) * 0.8)  # Calculate the 80% index
+        test_split_index = int(len(video_df) * 0.9)  # Calculate the 90% index
+        # Assign 'training' to the first 70% of rows
+        df.loc[video_df.index[:val_split_index], 'data_type'] = 'training'
+        # Assign 'validation' to the last 30% of rows
+        df.loc[video_df.index[val_split_index:test_split_index], 'data_type'] = 'validation'
+        # Assign 'test' to the last 50% of rows
+        df.loc[video_df.index[test_split_index:], 'data_type'] = 'test'
+    
+    return df
+
+
+######## Main Script ########
 
 input_data_dir = '/Users/jonaheaton/Documents/hulahoop_data/videos'
 input_data_list = os.listdir(input_data_dir)
@@ -58,7 +118,15 @@ for input_file in input_data_list:
 
 
 all_ellipse_data = pd.concat(all_ellipse_data_list)
+
+all_ellipse_data = assign_data_types(all_ellipse_data)
+
+print(all_ellipse_data['data_type'].value_counts())
+
+all_ellipse_data = determine_outlier_rows(all_ellipse_data, outlier_th=3)
+
 save_csv(all_ellipse_data, all_ellipse_data_file)
+
 
 
  
